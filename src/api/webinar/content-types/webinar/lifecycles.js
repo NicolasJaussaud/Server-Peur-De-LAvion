5ed4cf3daf4fea99fc248cd54c3d6201018949ab
NicolasJaussaud <jaussaudnc@gmail.com>
2022-07-03 15:33:30 +0200

    dev: change email api key

--- src/api/webinar/content-types/webinar/lifecycles.js
+++ src/api/webinar/content-types/webinar/lifecycles.js
@@ -7,7 +7,7 @@ module.exports = {
                 to: result.email,
                 from: 'noreply@peurdelavion.fr',
                 replyTo: 'noreply@peurdelavion.fr',
-                template_id: "d-9c48ef7f3d48497fb72d0e059944d583",
+                template_id: "d-7a4aeaddfb6e4e77aee447288b701044",
             }); 
         } catch(err) {
             console.log(err);
